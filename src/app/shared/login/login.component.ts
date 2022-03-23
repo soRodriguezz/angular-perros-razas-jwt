56d9477e514b230c158a2bb277bb155b5f4705df
soRodriguezz <s.rodriguezzapata01@gmail.com>
2022-03-22 22:45:46 -0300

    fix css

--- src/app/shared/login/login.component.ts
+++ src/app/shared/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 
 import Swal from 'sweetalert2';
 
 import { AuthService } from 'src/app/services/auth.service';
+import { Properties } from 'src/app/properties/properties';
 
 @Component({
   selector: 'app-login',
@@ -13,7 +14,7 @@ import { AuthService } from 'src/app/services/auth.service';
 })
 export class LoginComponent {
   public loginForm = this.fb.group({
-    email: [''],
+    email: ['', [Validators.pattern(Properties.VALIDATION_EMAIL)]],
     password: [''],
   });
 
@@ -24,18 +25,26 @@ export class LoginComponent {
   ) {}
 
   public login(): void {
-    this.authService.signin(this.loginForm.value).subscribe({
-      next: (data: any) => {
-        localStorage.setItem('token', data.token);
-        this.router.navigate(['/datos']);
-      },
-      error: (error: any) => {
-        Swal.fire({
-          icon: 'error',
-          title: 'Error',
-          text: error.error.message,
-        });
-      },
-    });
+    if( this.loginForm.valid) {
+      this.authService.signin(this.loginForm.value).subscribe({
+        next: (data: any) => {
+          localStorage.setItem('token', data.token);
+          this.router.navigate(['/datos']);
+        },
+        error: (error: any) => {
+          Swal.fire({
+            icon: 'error',
+            title: 'Error',
+            text: error.error.message,
+          });
+        },
+      });
+    } else {
+      Swal.fire({
+        icon: 'error',
+        title: 'Error',
+        text: 'Contraseña o correo incorrecto',
+      });
+    }
   }
 }
